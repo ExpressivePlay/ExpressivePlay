f7260c24b568acb805d37629ca64d88e882f7aa2
Jordan Oliveira <jordandossantosoliveira@hotmail.com>
2018-03-25 14:46:29 -0300

    Firebase instalado no projeto

---
+++ Assets/MyFireBase.cs
@@ -0,0 +1,36 @@
+﻿using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+using UnityEngine;
+
+public class MyFireBase : MonoBehaviour {
+	void Start() {
+		// Set up the Editor before calling into the realtime database.
+		FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://firstproject-a814b.firebaseio.com/");
+
+		// Get the root reference location of the database.
+		DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
+
+		writeNewUser ("1", "Jordan", "jordan@gmail.com");
+	}
+
+	private void writeNewUser(string userId, string name, string email) {
+		User user = new User (name, email);
+		string json = JsonUtility.ToJson (user);
+		print (json);
+	}
+
+}
+
+public class User {
+	public string username;
+	public string email;
+
+	public User() {
+	}
+
+	public User(string username, string email) {
+		this.username = username;
+		this.email = email;
+	}
+}
