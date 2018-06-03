7b945dbb922d9a2ce53ea93216211d03d8ec46ef
Jordan Oliveira <jordandossantosoliveira@hotmail.com>
2018-06-03 18:26:15 -0300

    Ajustes na Cena Chat / Correção de nivel duplicado

--- Assets/DragDrop/Scripts/GameChat/BlockGeneretorChat.cs
+++ Assets/DragDrop/Scripts/GameChat/BlockGeneretorChat.cs
@@ -38,6 +38,7 @@ public class BlockGeneretorChat : MonoBehaviour {
 						if (aux.blocos [0].tipo == GameControllerChat.Instance.panelFhaseChat.nextType [0]) {
 							foreach(Palavra auxPalavra in GameControllerChat.Instance.dataBasePalavras.palavras){
 								if (auxPalavra.tipo == aux.blocos [1].tipo) {
+									print (aux.id);
 									blocks.Add (auxPalavra);
 									auxBlocks.Add (auxPalavra);
 								}
@@ -50,6 +51,7 @@ public class BlockGeneretorChat : MonoBehaviour {
 						if (aux.blocos [0].tipo == GameControllerChat.Instance.panelFhaseChat.nextType [0]) {
 							foreach(Palavra auxPalavra in GameControllerChat.Instance.dataBasePalavras.palavras){
 								if (auxPalavra.tipo == aux.blocos [1].tipo) {
+									print (aux.id);
 									blocks.Add (auxPalavra);
 									auxBlocks.Add (auxPalavra);
 								}
@@ -62,6 +64,7 @@ public class BlockGeneretorChat : MonoBehaviour {
 						if (aux.blocos [0].tipo == GameControllerChat.Instance.panelFhaseChat.nextType [0]) {
 							foreach(Palavra auxPalavra in GameControllerChat.Instance.dataBasePalavras.palavras){
 								if (auxPalavra.tipo == aux.blocos [1].tipo) {
+									print (aux.id);
 									blocks.Add (auxPalavra);
 									auxBlocks.Add (auxPalavra);
 								}
@@ -74,6 +77,7 @@ public class BlockGeneretorChat : MonoBehaviour {
 						if (aux.blocos [0].tipo == GameControllerChat.Instance.panelFhaseChat.nextType [0]) {
 							foreach(Palavra auxPalavra in GameControllerChat.Instance.dataBasePalavras.palavras){
 								if (auxPalavra.tipo == aux.blocos [1].tipo) {
+									print (aux.id);
 									blocks.Add (auxPalavra);
 									auxBlocks.Add (auxPalavra);
 								}
@@ -86,6 +90,7 @@ public class BlockGeneretorChat : MonoBehaviour {
 						if (aux.blocos [0].tipo == GameControllerChat.Instance.panelFhaseChat.nextType [0]) {
 							foreach(Palavra auxPalavra in GameControllerChat.Instance.dataBasePalavras.palavras){
 								if (auxPalavra.tipo == aux.blocos [1].tipo) {
+									print (aux.id);
 									blocks.Add (auxPalavra);
 									auxBlocks.Add (auxPalavra);
 								}
